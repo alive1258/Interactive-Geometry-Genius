687eb11ccbf6c6dab3fea90dfefad44ccc57d87c
zamirulkabir <zamirulkabir999@gmail.com>
2023-02-18 19:43:47 +0600

    card mouse hover background color done

--- commonFc.js
+++ commonFc.js
@@ -48,3 +48,77 @@ function areaCalculation(names, total) {
   container.appendChild(tr);
 }
 // mouse hover color
+
+let color = [
+  "#3C9EE7",
+  "#E7993C",
+  "#a21caf",
+  "#67e8f9",
+  "#E73C99",
+  "#7e22ce",
+  "#be123c",
+  "#3CE746",
+  "#3f6212",
+  "#E7993C",
+  "#34d399",
+  "#78350f",
+];
+let colors = ["#ffff"];
+// cards-1
+document.getElementById("card-1").addEventListener("mouseenter", function () {
+  document.getElementById("card-1").style.background =
+    color[Math.floor(Math.random() * color.length)];
+});
+document.getElementById("card-1").addEventListener("mouseleave", function () {
+  document.getElementById("card-1").style.background =
+    colors[Math.floor(Math.random() * colors.length)];
+});
+// card-2
+
+document.getElementById("card-2").addEventListener("mouseenter", function () {
+  document.getElementById("card-2").style.background =
+    color[Math.floor(Math.random() * color.length)];
+});
+document.getElementById("card-2").addEventListener("mouseleave", function () {
+  document.getElementById("card-2").style.background =
+    colors[Math.floor(Math.random() * colors.length)];
+});
+// card-3
+document.getElementById("card-3").addEventListener("mouseenter", function () {
+  document.getElementById("card-3").style.background =
+    color[Math.floor(Math.random() * color.length)];
+});
+document.getElementById("card-3").addEventListener("mouseleave", function () {
+  document.getElementById("card-3").style.background =
+    colors[Math.floor(Math.random() * colors.length)];
+});
+
+// card-4
+
+document.getElementById("card-4").addEventListener("mouseenter", function () {
+  document.getElementById("card-4").style.background =
+    color[Math.floor(Math.random() * color.length)];
+});
+document.getElementById("card-4").addEventListener("mouseleave", function () {
+  document.getElementById("card-4").style.background =
+    colors[Math.floor(Math.random() * colors.length)];
+});
+// card-5
+document.getElementById("card-5").addEventListener("mouseenter", function () {
+  document.getElementById("card-5").style.background =
+    color[Math.floor(Math.random() * color.length)];
+});
+document.getElementById("card-5").addEventListener("mouseleave", function () {
+  document.getElementById("card-5").style.background =
+    colors[Math.floor(Math.random() * colors.length)];
+});
+// card-6
+document.getElementById("card-6").addEventListener("mouseenter", function () {
+  document.getElementById("card-6").style.background =
+    color[Math.floor(Math.random() * color.length)];
+});
+document.getElementById("card-6").addEventListener("mouseleave", function () {
+  document.getElementById("card-6").style.background =
+    colors[Math.floor(Math.random() * colors.length)];
+});
+

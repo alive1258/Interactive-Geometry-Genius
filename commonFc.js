2ca912555d3fe5e9f8f12144254ac27cd0e15004
zamirulkabir <zamirulkabir999@gmail.com>
2023-02-18 18:57:10 +0600

    Rectangle card calculation done

--- commonFc.js
+++ commonFc.js
@@ -17,6 +17,7 @@ function getSecondInputFieldValueById(secondInputFieldId) {
   return secondInputFieldValue;
 }
 
+
 //   all areaCalculation
 
 function areaCalculation(names, total) {
--- interactive.js
+++ interactive.js
@@ -26,5 +26,31 @@ document
     areaCalculation(productName, totalPrice.toFixed(2));
   });
 
+// rectangle
+document
+  .getElementById("rectangleCalculate")
+  .addEventListener("click", function () {
+    serial += 1;
 
+    const productName = document.getElementById("title-2").innerText;
 
+    const firstInputTriangleField = getFirstInputFieldValueById(
+      "Rectangle-first-input"
+    );
+
+    const secondInputTriangleField = getSecondInputFieldValueById(
+      "Rectangle-second-input"
+    );
+
+    const totalPrice = firstInputTriangleField * secondInputTriangleField;
+    if (
+      isNaN(totalPrice) ||
+      totalPrice <= 0 ||
+      totalPrice == "" ||
+      totalPrice !== "number"
+    ) {
+      alert("please provide a valid number");
+      return;
+    }
+    areaCalculation(productName, totalPrice.toFixed(2));
+  });
